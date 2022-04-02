b164fe352fa9cb28e1bd1be3118818da69f14ea6
Artem Shevchuk <pilot0shevchuk@gmail.com>
2022-04-02 16:16:59 +0300

    get current user

--- hookproject/src/index.js
+++ hookproject/src/index.js
@@ -5,14 +5,17 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import Routes from './node_modules/pages/routes'
 import TopBar from './node_modules/components/topBar'
 import {CurrentUserProvider} from './node_modules/contexts/currentUser'
+import CurrentUserChecker from './node_modules/components/currentUserChecker'
 
 const App = () => { 
     return (
         <CurrentUserProvider>
+            <CurrentUserChecker>
             <Router>
                 <TopBar/>
                 <Routes/>
             </Router>
+            </CurrentUserChecker>
         </CurrentUserProvider>
     )
 }
